29e9ea5afb943d88c7cca0d4cb115140847dd36a
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-08-29 18:51:16 +0530

    Add files via upload

---
+++ EmployeeAPICRUD2/src/main/java/com/Sharma/EmployeeApicrud2Application.java
@@ -0,0 +1,13 @@
+package com.Sharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApicrud2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApicrud2Application.class, args);
+	}
+
+}
---
+++ EmployeeAPICRUD2/src/test/java/com/Sharma/EmployeeApicrud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApicrud2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
